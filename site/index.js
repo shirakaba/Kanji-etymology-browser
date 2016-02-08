abed5535930349cbd2ebf893ad2c0156ec59d3a1
Jamie Birch <jbizzle5@gmail.com>
2016-02-08 21:58:37 +0000

    Beginning to migrate from sampleapp.js to index.js.

---
+++ site/index.js
@@ -0,0 +1,7 @@
+// npm init [makes package.json]
+// npm install express --save
+// [creates node_modules folder if necessary, then adds express to node_modules folder;]
+//[--save adds the dependency upon 'express' to the package.json]
+
+
+// npm install [would install everything specified by the package.json, if a package.json were present]
