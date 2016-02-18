13b713e1d3db3517cc6dcdb946f158ea23e158ff
Jamie Birch <jbizzle5@gmail.com>
2016-02-18 01:06:30 +0000

    Can now send json data through form to modify page without refreshing

--- site/index.js
+++ site/index.js
@@ -1,10 +1,10 @@
-// npm init [makes package.json]
-// npm install express --save
-// npm install sqlite3 --save
-// npm install body-parser --save
-// [installs the sqlite3 module (confusingly also called sqlite3) and adds it into the package.json as a required install]
-// [creates node_modules folder if necessary, then adds express to node_modules folder;]
-// [--save adds the installed dependency to the package.json as a required install for use by 'npm install']
+/* npm init [makes package.json]
+ * npm install express --save
+ * npm install sqlite3 --save
+ * npm install body-parser --save
+ * [installs the sqlite3 module (confusingly also called sqlite3) and adds it into the package.json as a required install]
+ * [creates node_modules folder if necessary, then adds express to node_modules folder;]
+ * [--save adds the installed dependency to the package.json as a required install for use by 'npm install'] */
 
 
 // npm install [would install everything specified by the package.json, if a package.json were present]
@@ -32,16 +32,17 @@ app.get('/', function(request, response){
             console.error(err);
             return;
         }
-        //to write rows into console
-        //console.log(row);
-        //Note: can only send one request to webpage at a time (not LIMIT 10), so need a way to collect them into one request for display.
-        // and can only have one response.send. Originally had response.send(row);
-        //var data = JSON.stringify(row, null, "\t");
-        // formats the data to be sent as code with <pre>
-        //var formatted = util.format("<pre>%s</pre>", data);
-        //response.send(formatted);
+        /* to write rows into console
+         * console.log(row);
+         * Note: can only send one request to webpage at a time (not LIMIT 10), so need a way to collect them into one
+         * request for display.
+         *  and can only have one response.send. Originally had response.send(row);
+         * var data = JSON.stringify(row, null, "\t");
+         *  formats the data to be sent as code with <pre>
+         * var formatted = util.format("<pre>%s</pre>", data);
+         * response.send(formatted);
 
-        //response.setHeader('Content-Type', 'application/json');
+         * response.setHeader('Content-Type', 'application/json'); */
         response.json(row);
     });
 });
@@ -49,43 +50,36 @@ app.get('/', function(request, response){
 // Good tutorial if we ditch the HTML button: https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
 // Creating and handling from start to finish: http://www.sitepoint.com/creating-and-handling-forms-in-node-js/
 app.get('/html/kanjisearch', function(request, response){
-
-        db.all("SELECT" +  + "FROM henshall_page LIMIT 10", function(err, row){
-        //db.each("SELECT * FROM henshall_page LIMIT 10", function(err, row){
+        db.each("SELECT * FROM henshall_page LIMIT 10", function(err, row){
             if(err){
                 console.error(err);
                 return;
             }
-            //to write rows into console
-            //console.log(row);
-            //Note: can only send one request to webpage at a time (not LIMIT 10), so need a way to collect them into one request for display.
-            // and can only have one response.send. Originally had response.send(row);
-            //var data = JSON.stringify(row, null, "\t");
-            // formats the data to be sent as code with <pre>
-            //var formatted = util.format("<pre>%s</pre>", data);
-            //response.send(formatted);
-
-            //response.setHeader('Content-Type', 'application/json');
             response.json(row);
         });
 });
 
 
-/*
 // mysql not imported. Ref: http://www.hacksparrow.com/using-mysql-with-node-js.html
 // sqlite3 nodejs documentation: https://github.com/mapbox/node-sqlite3/wiki/API or https://www.npmjs.com/package/sqlite3
-app.get('/', function(request, response){
-db.run("SELECT * FROM henshall_page LIMIT 1");
-response.json(row);
-});
-*/
 
 // Following this guide: www.expressjs.com/en/guide/routing.html
 app.post('/', function(request, response){
-	console.log(request.body.a);
-   response.json({
-   	"sup": "yo"
-   });
+	console.log(request.body.oursearch);
+  
+	// db.prepare("SELECT * FROM henshall_page WHERE hkanji = (?) LIMIT 10", request.body.oursearch)
+	db.prepare("SELECT * FROM henshall_page LIMIT 10")
+	.all(function(err, row){
+        //db.each("SELECT " + "\"request.body.oursearch\"" + "FROM henshall_page LIMIT 10", function(err, row){
+            if(err){
+                console.error(err);
+                return;
+            }
+
+            response.json({
+   				"receivedsearch": request.body.oursearch, "row":row
+   			});
+        });
 });
 
 
@@ -94,9 +88,9 @@ app.listen(3000, function (){
    console.log('yo');
 });
 
-// Creates a mapping between your filesystem, and the filesystem you pretend exists.
-// If you ask for the first parameter to be just '', then no extra folder is inserted in front of the 'images' in the URL presented to the user.
-// In this case: We can access file://...site/images/index.html at:
-// http://localhost:3000/index.html
-// Rules that the images folder in site/ needs to be presented in URLs as nothing at all.
+/* Creates a mapping between your filesystem, and the filesystem you pretend exists.
+ * If you ask for the first parameter to be just '', then no extra folder is inserted in front of the 'images' in the URL presented to the user.
+ * In this case: We can access file://...site/images/index.html at:
+ * http://localhost:3000/index.html
+ * Rules that the images folder in site/ needs to be presented in URLs as nothing at all. */
 app.use('', express.static('resources'));
